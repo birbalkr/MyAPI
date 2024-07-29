f0b3a5c808586d6aec442674957c38011610d10e
birbalkr <birbalkr1435@gmail.com>
2024-07-29 17:09:58 +0530

    create api no data add and delete only GET method work

--- PythonAPI/FlaskApi/app.py
+++ PythonAPI/FlaskApi/app.py
@@ -1,18 +1,44 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app=Flask(__name__)
 
+book_list=[
+    {
+        'id':0,
+        'title':'MyBook',
+        'language':'Python',
+    },
+    {
+        'id':0,
+        'title':'MyBook',
+        'language':'Python',
+    }
+]
+
+@app.route('/books',methods=['GET','PUT'])
+def books():
+    if request.method=='GET':
+        if len(book_list)>0:
+            return jsonify(book_list)
+        else:
+            'Nothing Fount',404
+    if request.method=='POST':
+        new_title=request.form['title']
+        new_lang=request.form['language']
+        iD=book_list[-1]['id']+1
+
+        new_obj={
+            'id':iD,
+            'title':new_title,
+            'language':new_lang
+        }
+        book_list.append(new_obj)
+        return jsonify(book_list),201
+    
+
 @app.route('/')
 def index():
-    return 'hello' 
+    return 'hello'
 
-# progammatically
 if __name__=='__main__':
     app.run()
-# run 
-# python app.py
-
-# Dynamic Routing
-@app.route('/<name>')
-def print_name(name):
-    return 'Hii, {}'.format(name)
