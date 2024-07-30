6c25783cd52f1947a148885f08f2ab46186780c7
birbalkr <birbalkr1435@gmail.com>
2024-07-30 18:11:18 +0530

    Create Schema : Model folder

--- NodeJs_API/db/connect.js
+++ NodeJs_API/db/connect.js
@@ -3,16 +3,14 @@ const mongoose = require('mongoose');
 // uri=
 // 'mongodb+srv://birbalkr1435:Birbal!@#123@myappapi.eqfuppm.mongodb.net/myappapi?retryWrites=true&w=majority&appName=myappapi'
 
-uri = "mongodb+srv://birbalkr1435:AkjV672rvjrL2Omi@myappapi.eqfuppm.mongodb.net/myappapi?retryWrites=true&w=majority&appName=myappapi"
 
 
-
-const connectDB=()=>{
+const connectDB=(uri)=>{
     console.log('db');
     return mongoose.connect(uri, {
         useNewUrlParser:true,
         useUnifiedTopology:true,
-    })
+    });
 };
 
 module.exports = connectDB;
