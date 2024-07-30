86be6650aa31a536e51fa60f5e2e511822262c90
birbalkr <birbalkr1435@gmail.com>
2024-07-30 21:21:10 +0530

    find Data on api : add some line in controllers/products

--- NodeJs_API/controllers/products.js
+++ NodeJs_API/controllers/products.js
@@ -1,5 +1,7 @@
+const Product= require('../models/product');
 const getAllProducts = async (req, res)=>{
-    res.status(200).json({msg:'I am getAllProducts'})
+    const myData = await Product.find({});
+    res.status(200).json(myData);
 }
 const getAllProductsTesting = async(req,res)=>{
     res.status(200).json({msg:'I am getAllProductsTesting'})
